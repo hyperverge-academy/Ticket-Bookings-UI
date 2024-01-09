4d176193cafafddcefccbff4c82151ad5dccb100
SAYALI <sayalighadigaonkar22@navgurukul.org>
2024-01-09 16:52:17 +0530

    modified-frontend

---
+++ js/login.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById("login-form");
+    const output = document.getElementById("output");
+  
+    document.getElementById("submit-btn").addEventListener("click", async function (event) {
+      event.preventDefault()
+        const phone = document.getElementById("phone").value;
+        const password = document.getElementById("password").value;
+  
+        const data = {
+          mobileNumber : phone,
+          password : password,
+        }
+        console.log(data);
+  
+        try {
+            const res = await fetch(
+                'http://localhost:3000/users/login',
+                {
+                    method: 'POST',
+                    body: JSON.stringify(data),
+                    headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json'
+            
+                },
+              }
+              );
+  
+            const resData = await res.json();
+            console.log(resData);
+  
+            const responseStatusCode = document.getElementById("response-statuscode").innerHTML = resData.message;
+            if ( resData.errorCode!== 200) {
+            document.getElementById('response-statuscode').style.color = 'red';
+            } return responseStatusCode
+
+  
+        } catch (err) {
+            console.log(err.message);
+        }
+    });
+  
+  
+    });
---
+++ js/registration.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const form = document.getElementById("registration-form");
+  const output = document.getElementById("output");
+
+  document.getElementById("submit-btn").addEventListener("click", async function (event) {
+    event.preventDefault()
+      const fullName = document.getElementById("fullName").value;
+      const phone = document.getElementById("phone").value;
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirmPassword").value;
+
+      const data = {
+        fullName : fullName,
+        mobileNumber : phone,
+        password : password,
+        confirmPassword : confirmPassword
+      }
+      console.log(data);
+
+      try {
+          const res = await fetch(
+              'http://localhost:3000/users/registration',
+              {
+                  method: 'POST',
+                  body: JSON.stringify(data),
+                  headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'          
+              },
+            }
+            );
+
+          const resData = await res.json();
+          console.log(resData);
+
+          const responseStatusCode = document.getElementById("response-statuscode").innerHTML = resData.message;
+          if ( resData.errorCode!== 200) {
+          document.getElementById('response-statuscode').style.color = 'red';
+          } return responseStatusCode
+
+
+      } catch (err) {
+          console.log(err.message);
+      }
+  });
+
+
+  });
